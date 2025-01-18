591ef89e14aadeababeffa97781e320ecfa9ff56
Hieu <haminhhieu1005v@gmail.com>
2025-01-18 17:28:00 +0700

    fix

--- Model/Gen_CVRPTW_data.py
+++ Model/Gen_CVRPTW_data.py
@@ -11,17 +11,24 @@ from torch_geometric.data import Data
 # device =  torch.device("cuda:0" if True == True else "cpu")
 device = None
 
-TW_CAPACITIES = {
-        10: 250.,
-        20: 500.,
-        50: 750.,
-        100: 1000.,
-        200: 1500.,
-        400: 2000.
-    }
+
+def gen_distance_matrix(tsp_coordinates, device):
+    n_nodes = len(tsp_coordinates)
+    distances = torch.norm(tsp_coordinates[:, None] - tsp_coordinates, dim=2, p=2)
+    return distances
 
 
 
+# device = None 
+
+size = 1
+time_factor = 100
+service_duration = 10
+tw_expansion = 3
+service_window = 1000
+loc = 15
+scale = 10
+max_ = 42
 
 CVRPTW_SET = namedtuple("CVRPTW_SET",
                         ["depot_loc",    # Depot location
@@ -31,71 +38,56 @@ CVRPTW_SET = namedtuple("CVRPTW_SET",
                          "depot_tw",     # depot time window (full horizon)
                          "node_tw",      # node time windows
                          "durations",    # service duration per node
-                         "service_window",  # maximum of time units
-                         "time_factor"])    # value to map from distances in [0, 1] to time units (transit times)
+                        ])   
 
+TW_CAPACITIES = {
+        20: 70.,
+        50: 100.,
+        100: 200.,
+        150: 500.,
+        200: 700. 
+    }
 
-
-
-def generate_cvrptw_data(cfg, rnds=None,
-                         ):
-    """Generate data for CVRP-TW
-
-    Args:
-        size (int): size of dataset
-        graph_size (int): size of problem instance graph (number of customers without depot)
-        rnds : numpy random state
-        service_window (int): maximum of time units
-        service_duration (int): duration of service
-        time_factor (float): value to map from distances in [0, 1] to time units (transit times)
-        tw_expansion (float): expansion factor of TW w.r.t. service duration
-
-    Returns:
-        List of CVRP-TW instances wrapped in named tuples
-    """
-    rnds = np.random if rnds is None else rnds
-
+def generate_cvrptw_data(graph_size = 100):
     # sample locations
-    dloc = rnds.uniform(size=(cfg.size, 2))  # depot location
-    nloc = rnds.uniform(size=(cfg.size, cfg.graph_size, 2))  # node locations
+    rnds = np.random
+    dloc = rnds.uniform(size=(size, 2))  # depot location
+    nloc = rnds.uniform(size=(size, graph_size, 2))  # node locations
 
     # TW start needs to be feasibly reachable directly from depot
-    min_t = np.ceil(np.linalg.norm(dloc[:, None, :]*cfg.time_factor - nloc*cfg.time_factor, axis=-1)) + 1
+    min_t = np.ceil(np.linalg.norm(dloc[:, None, :]*time_factor - nloc*time_factor, axis=-1)) + 1
     # TW end needs to be early enough to perform service and return to depot until end of service window
-    max_t = np.ceil(np.linalg.norm(dloc[:, None, :]*cfg.time_factor - nloc*cfg.time_factor, axis=-1) + cfg.service_duration) + 1
+    max_t = np.ceil(np.linalg.norm(dloc[:, None, :]*time_factor - nloc*time_factor, axis=-1) + service_duration) + 1
 
     # horizon allows for the feasibility of reaching nodes / returning from nodes within the global tw (service window)
-    horizon = list(zip(min_t, cfg.service_window - max_t))
-    epsilon = np.maximum(np.abs(rnds.standard_normal([cfg.size, cfg.graph_size])), 1 / cfg.time_factor)
+    horizon = list(zip(min_t, service_window - max_t))
+    epsilon = np.maximum(np.abs(rnds.standard_normal([size, graph_size])), 1 / time_factor)
 
     # sample earliest start times a
     a = [rnds.randint(*h) for h in horizon]
 
     tw = [np.transpose(np.vstack((rt,  # a
-                                  np.minimum(rt + cfg.tw_expansion * cfg.time_factor * sd, h[-1]).astype(int)  # b
+                                  np.minimum(rt + tw_expansion * time_factor * sd, h[-1]).astype(int)  # b
                                   ))).tolist()
           for rt, sd, h in zip(a, epsilon, horizon)]
 
-
-    return [CVRPTW_SET(*data) for data in zip(
-        torch.tensor(dloc.tolist(), device = device) * cfg.time_factor,
-        torch.tensor(nloc.tolist(), device = device) * cfg.time_factor,
-        torch.tensor(np.minimum(np.maximum(np.abs(rnds.normal(loc=cfg.loc, scale=cfg.scale, size=[cfg.size, cfg.graph_size])).astype(int), 1), cfg.max).tolist(), device = device),
-        torch.tensor(np.full(cfg.size, TW_CAPACITIES[cfg.graph_size]).tolist(), device = device),
-        torch.tensor([[0, cfg.service_window]] * cfg.size, dtype = torch.float32, device = device),
+    a = [CVRPTW_SET(*data) for data in zip(
+        torch.tensor(dloc.tolist(), device = device) * time_factor,
+        torch.tensor(nloc.tolist(), device = device) * time_factor,
+        torch.tensor(np.minimum(np.maximum(np.abs(rnds.normal(loc=loc, scale=scale, size=[size, graph_size])).astype(int), 1), max_).tolist(), device = device),
+        torch.tensor(np.full(size, TW_CAPACITIES[graph_size]).tolist(), device = device),
+        torch.tensor([[0, service_window]] * size, dtype = torch.float32, device = device),
         torch.tensor(tw, dtype = torch.float32, device = device),
-        torch.tensor(np.full([cfg.size, cfg.graph_size], cfg.service_duration).tolist(), device = device),
-        torch.tensor([cfg.service_window] * cfg.size, device = device),
-        torch.tensor([cfg.time_factor] * cfg.size, device = device),
+        torch.tensor(np.full([size, graph_size], service_duration).tolist(), device = device)
     )]
+    return a[0]
 
 
-def gen_distance_matrix(tsp_coordinates, device):
-    n_nodes = len(tsp_coordinates)
-    distances = torch.norm(tsp_coordinates[:, None] - tsp_coordinates, dim=2, p=2)
-    return distances
 
-def gen_pyg_data(cfg, demands, time_window, durations, service_window, time_factor, distances, device):
+
+
+
+def gen_pyg_data(demands, time_window, durations, distances, device, scale = 1000.0):
     n = demands.size(0)
     nodes = torch.arange(n, device=device)
     u = nodes.repeat(n)
@@ -105,10 +97,10 @@ def gen_pyg_data(cfg, demands, time_window, durations, service_window, time_fact
     x = demands
     x = torch.where(x == 0, 1e-7, x)
     edge_attr = torch.where(edge_attr == 0, 1e-7, edge_attr)
-    pyg_data = Data(x=x.unsqueeze(1), edge_attr=edge_attr, edge_index=edge_index, time_window = time_window, durations = durations, service_window = service_window, time_factor = time_factor)
+    pyg_data = Data(x=x.unsqueeze(1), edge_attr=edge_attr, edge_index=edge_index, time_window = time_window, durations = durations)
     return pyg_data
 
-def gen_pyg_data_normalize(cfg, demands, time_window, durations, service_window, time_factor, distances, cap, device, scale = 1000.0):
+def gen_pyg_data_normalize(demands, time_window, durations, distances, device, scale = 1000.0):
     n = demands.size(0)
     nodes = torch.arange(n, device=device)
     u = nodes.repeat(n)
@@ -118,6 +110,7 @@ def gen_pyg_data_normalize(cfg, demands, time_window, durations, service_window,
     x = demands
     x = torch.where(x == 0, 1e-7, x)
     edge_attr = torch.where(edge_attr == 0, 1e-7, edge_attr)
-    pyg_data = Data(x=x.unsqueeze(1)/cap, edge_attr=edge_attr/scale, edge_index=edge_index, time_window = time_window/scale, durations = durations/scale, service_window = service_window, time_factor = time_factor)
+    capacity = TW_CAPACITIES[n-1]
+    pyg_data = Data(x=x.unsqueeze(1) / capacity, edge_attr=edge_attr/scale, edge_index=edge_index, time_window = time_window/scale, durations = durations/scale)
     return pyg_data
 
--- Normalize_data.py
+++ Normalize_data.py
@@ -5,18 +5,16 @@ from Model.Gen_CVRPTW_data import *
 
 device = None 
 
-def normalize_data(cfg):
+def normalize_data():
     max_cap, xcoord, ycoord, demand, e_time, l_time, s_time, data = load_data()
     data = torch.tensor([[float(x) for x in y] for y in data])
     tsp_coordinates = data[:, 1:3] 
     demands = torch.tensor(demand, dtype = torch.float32)
     time_window = data[:, 4:]
     durations = time_window[:, -1] 
-    service_window = cfg.service_window
-    time_factor = cfg.time_factor
     distances = gen_distance_matrix(tsp_coordinates, device = device)
 
 
-    pyg_data_normalize = gen_pyg_data_normalize(cfg, demands, time_window, durations, service_window, time_factor, distances, max_cap, device, scale = 1000.0)
+    pyg_data_normalize = gen_pyg_data_normalize(demands, time_window, durations, distances, device, scale = 1000.0)
     return pyg_data_normalize
 
